e30f37310c5c017733f1f1a6c51e73e06e9d042e
GrandmasterShadowMorgue <jayant.shivarajan@gmail.com>
2017-06-09 22:00:08 +0100

    Added Volunteer and Wish classes

---
+++ Make-a-wish/app/src/main/java/com/team11/makeawish/make_a_wish/logic/Volunteer.java
@@ -0,0 +1,52 @@
+package com.team11.makeawish.make_a_wish.logic;
+
+import java.util.List;
+
+/**
+ * Created by jayant on 09/06/2017.
+ */
+
+public class Volunteer {
+    String location, name, contact;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    List<String> skills;
+
+    public Volunteer(String location, String name, String contact, List<String> skills) {
+        this.location = location;
+        this.name = name;
+        this.contact = contact;
+        this.skills = skills;
+    }
+}
---
+++ Make-a-wish/app/src/main/java/com/team11/makeawish/make_a_wish/logic/Wish.java
@@ -0,0 +1,43 @@
+package com.team11.makeawish.make_a_wish.logic;
+
+import java.util.List;
+
+/**
+ * Created by jayant on 09/06/2017.
+ */
+
+public class Wish {
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public List<String> getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(List<String> requirements) {
+        this.requirements = requirements;
+    }
+
+    String target;
+    boolean isDone;
+    List<String> requirements;
+
+    public Wish(String target, boolean isDone, List<String> requirements) {
+        this.target = target;
+        this.isDone = isDone;
+        this.requirements = requirements;
+    }
+}
