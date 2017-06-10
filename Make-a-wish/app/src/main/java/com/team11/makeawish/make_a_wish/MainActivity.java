c2d86144718c3aed1712fb2bb6413215159c234d
eugene <eugene.maina@student.manchester.ac.uk>
2017-06-10 03:33:23 +0100

    Main login handling

--- Make-a-wish/app/src/main/java/com/team11/makeawish/make_a_wish/MainActivity.java
+++ Make-a-wish/app/src/main/java/com/team11/makeawish/make_a_wish/MainActivity.java
@@ -2,12 +2,95 @@ package com.team11.makeawish.make_a_wish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name, password;
+    String Name, Password;
+    Context ctx=this;
+    String NAME=null, PASSWORD=null, EMAIL=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        name = (EditText) findViewById(R.id.main_name);
+        password = (EditText) findViewById(R.id.main_password);
+    }
+
+    public void main_register(View v){
+        startActivity(new Intent(this,Register.class));
+    }
+
+    public void main_login(View v){
+        Name = name.getText().toString();
+        Password = password.getText().toString();
+        BackGround b = new BackGround();
+        b.execute(Name, Password);
+    }
+
+    class BackGround extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String name = params[0];
+            String password = params[1];
+            String data="";
+            int tmp;
+
+            try {
+                Connection connection = null;
+                connection = DriverManager.getConnection("jdbc:mysql://hostname:3306/makeawish", "root", "");
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            String err=null;
+            try {
+                JSONObject root = new JSONObject(s);
+                JSONObject user_data = root.getJSONObject("user_data");
+                NAME = user_data.getString("name");
+                PASSWORD = user_data.getString("password");
+            } catch (JSONException e) {
+                e.printStackTrace();
+                err = "Exception: "+e.getMessage();
+            }
+
+            Intent i = new Intent(ctx, Home.class);
+            i.putExtra("name", NAME);
+            i.putExtra("password", PASSWORD);
+            i.putExtra("err", err);
+            startActivity(i);
+        }
     }
 }
