750a00dd07669ebebcb2ce0c9a667ca7702aff3b
kelennaa <kelennaa@yahoo.com>
2017-06-10 04:00:10 +0100

    first page activity

--- Make-a-wish/app/src/main/java/com/team11/makeawish/make_a_wish/FirstActivity.java
+++ Make-a-wish/app/src/main/java/com/team11/makeawish/make_a_wish/FirstActivity.java
@@ -1,8 +1,36 @@
 package com.team11.makeawish.make_a_wish;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by kelennaamadi on 10/06/2017.
  */
 
-public class FirstActivity {
+public class FirstActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_page);
+
+        Button loginbutton = (Button) findViewById(R.id.loginButton);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+/**intent*/
+            }
+        });
+
+        Button registerbutton = (Button) findViewById(R.id.registerButton);
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+/**intent*/
+            }
+        });
+
+    }
 }
+
